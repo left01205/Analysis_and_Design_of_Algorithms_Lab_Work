408bf0f193d04994c50e1bd97df0b4c15bc7c064
left01205 <student4.ai@csvtu.ac.in>
2024-11-24 14:15:25 +0530

    Maximum sum of subarray

---
+++ Max_sum_sub_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <limits.h>
+
+// Function to find the maximum sum subarray
+int maxSubArraySum(int arr[], int size) {
+    int max_so_far = INT_MIN, max_ending_here = 0;
+
+    for (int i = 0; i < size; i++) {
+        max_ending_here = max_ending_here + arr[i];
+        if (max_so_far < max_ending_here)
+            max_so_far = max_ending_here;
+
+        if (max_ending_here < 0)
+            max_ending_here = 0;
+    }
+    return max_so_far;
+}
+
+int main() {
+    int arr[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int max_sum = maxSubArraySum(arr, n);
+    printf("Maximum sum of the subarray is %d\n", max_sum);
+    return 0;
+}
